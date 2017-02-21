7ae4a0cb6a5563441d7b33d8de61fcd68a2c1c87
Qing Zhou <qing.zhou@rea-group.com>
2017-02-21 14:52:50 +0800

    introduce into sinon to mock function and add jsom-global to provide global dom for mount

--- src/lib/Todo.js
+++ src/lib/Todo.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Todo = React.createClass({
+    componentDidMount: function() {
+        console.log('hhahhahha');
+    },
+
     render: function() {
         const { todo } = this.props;
 
--- test/lib/Todo-test.js
+++ test/lib/Todo-test.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Todo } from '../../src/lib/Todo';
 import { expect } from 'chai';
+import sinon from 'sinon';
+import 'jsdom-global/register';
 
 describe('Todo', () => {
     it('should render a div with "todo" class', () => {
-        const todo = 'Walk dog';
         const component = shallow(
             <Todo />
         );
 
         expect(component.find('.todo')).to.have.length(1);
+    });
+
+    it('calls componentDidMount', () => {
+      sinon.spy(Todo.prototype, 'componentDidMount');
+      const wrapper = mount(<Todo />);
+      expect(Todo.prototype.componentDidMount.calledOnce).to.equal(true);
     });
 });
