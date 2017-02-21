57d8244092a4225598ea12871b921a900e5c7ef0
Qing Zhou <qing.zhou@rea-group.com>
2017-02-21 14:29:34 +0800

    introduce enzyme to test

--- test/lib/Todo-test.js
+++ test/lib/Todo-test.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import {
-    renderIntoDocument,
-    findRenderedDOMComponentWithClass
-} from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 import { Todo } from '../../src/lib/Todo';
-import jsdom from 'jsdom';
 import { expect } from 'chai';
-
-const doc = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
-const win = doc.defaultView;
-
-global.document = doc;
-global.window = win;
 
 describe('Todo', () => {
     it('should render a div with "todo" class', () => {
         const todo = 'Walk dog';
-        const component = renderIntoDocument(
+        const component = shallow(
             <Todo />
         );
-        const todoEle = findRenderedDOMComponentWithClass(component, 'todo');
 
-        expect(todo).to.be.ok;
-    });
-
-    it('should render a div with the todo text', () => {
-        const todo = 'Walk dog';
-        const component = renderIntoDocument(
-            <Todo
-                todo={todo}
-                />
-        );
-        const todoEle = findRenderedDOMComponentWithClass(component, 'todo');
-        const todoText = todoEle.textContent;
-
-        expect(todoText).to.equal('Walk dog');
+        expect(component.find('.todo')).to.have.length(1);
     });
 });
