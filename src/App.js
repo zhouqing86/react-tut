ea27ce4cdec0741d9fbef9eb87b127cde314f88c
Qing Zhou <qing.zhou@rea-group.com>
2017-05-28 17:08:11 +0800

    update react.component as react.createclass

--- src/App.js
+++ src/App.js
@@ -70,16 +70,28 @@ class App extends React.Component {
   }
 }
 
-class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
-      );
-   }
-}
+// class TableRow extends React.Component {
+//    render() {
+//       return (
+//          <tr>
+//             <td>{this.props.data.id}</td>
+//             <td>{this.props.data.name}</td>
+//             <td>{this.props.data.age}</td>
+//          </tr>
+//       );
+//    }
+// }
+
+const TableRow = React.createClass({
+  render() {
+     return (
+        <tr>
+           <td>{this.props.data.id}</td>
+           <td>{this.props.data.name}</td>
+           <td>{this.props.data.age}</td>
+        </tr>
+     );
+  }
+});
 
 export default App;
